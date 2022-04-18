4cdbb2350c0c0b61ee48ba02b96318f99e4516d7
Yuri <97105831+Yuri3358@users.noreply.github.com>
2022-04-18 19:54:18 -0300

    Melhor comentado
    Explicações mais detalhadas vão estar no readme.md

--- testetkvideo.py
+++ testetkvideo.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from download import baixar
+from download import baixar #Toda a função de baixar o vídeo em um diretório escolhido está no import
 class App():
 
     def __init__(self):
@@ -10,7 +10,7 @@ class App():
         self.root.configure(background=f'{cor}')
         self.root.title('Conversor Youtube para audio')
 
-        label_videocode = Label(text='ID do vídeo', font='Arial 18', background=cor)
+        label_videocode = Label(text='ID do vídeo', font='Arial 18', background=cor) #ID do vídeo são os códigos que aparecem (Melhor explicações no README.md
         label_videocode.pack()
         
         entrada_id = Entry(self.root, font='Arial 14', width=40)
@@ -18,6 +18,7 @@ class App():
 
         label_mensagem = Label(self.root, text='', background=cor, font=20)
 
+        #contornei o problema da função em command, usando funções lambda
         submit = Button(text='Baixar', command=lambda: baixar(id=entrada_id, labelmessage=label_mensagem), height=2, width=20, pady=10, font=36, background='#636363')
         submit.pack()
 
